c616e7197f7182820e8e018363ed46174701494b
MohammadSenanAli <mdsenan@gmail.com>
2023-08-27 22:01:36 +0530

    Minor changes to the main.py file

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 # Import Pygame
 import pygame
+import sys
 
 # Import all the constants to this file
 from constant import *
 
+# from game import Game
+
 
 class Main:
     def __init__(self) -> None:
-        # Always initialize the pygame 
+        # Always initialize the pygame
         pygame.init()
-        pass
+        pygame.display.set_caption("Chess Engine - Mohammad Senan Ali")
+
+        # Initialising the screen for the pygame display main window
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    def game_loop(self):
+        screen = self.screen
+
+        while True:
+            # Event Handlers
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                elif event == pygame.MOUSEBUTTONDOWN:
+                    print("clicked")
+                    
+            list_item = ['a','b','c','d']
+            for i in list_item:
+                print(list_item[i])
+
+            pygame.display.update()
 
 
-main = Main().game_loop()
-
-if "__name__" == "__main__":
-    main()
+if __name__ == "__main__":
+    main = Main()
+    main.game_loop()
